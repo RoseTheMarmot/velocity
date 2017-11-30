bf5421c8aa68b40bb79b5f0b135d981d1810de1f
RoseTheMarmot <rose.beede@gmail.com>
2017-11-30 14:23:45 -0800

    styled header

--- header.php
+++ header.php
@@ -25,13 +25,14 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'velocity' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
+		<div class="site-branding" style="<?php echo 'background-image:url('.get_header_image().');';?>">
 			<?php
 			the_custom_logo();
+			$title_style = empty(get_theme_mod( 'custom_logo' )) ? '' : 'hidden';
 			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title <?php echo $title_style;?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title <?php echo $title_style;?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php
 			endif;
 
